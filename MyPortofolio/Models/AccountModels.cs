4766ae4a0921a4c6006f2c87c99121d43f17dd9a
moussaballa <cifadel1@hotmail.com>
2017-06-13 23:46:50 -0400

    Boostrapandloginfix

--- MyPortofolio/Models/AccountModels.cs
+++ MyPortofolio/Models/AccountModels.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Data.SqlClient;
 
 namespace MyPortofolio.Models
 {
